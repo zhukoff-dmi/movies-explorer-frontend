39a09a7aac5083a35b5e24da09f316a4bad8d9d4
zhukoff.dmi <happy-beard@mail.ru>
2023-09-03 01:13:48 +0300

    Функционал на React

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.zhukoffdmi.nomoreparties.co";
 
 const getJson = (res) => {
     if (res.ok) {
--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -73,7 +73,7 @@ class MainApi {
 }
 
 const mainApi = new MainApi({
-    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-61',
+    baseUrl: 'https://api.zhukoffdmi.nomoreparties.co',
     headers: {
         authorization: '7e54c718-5681-4ba1-ab1c-120b9ede7317',
         'Content-Type': 'application/json'
