0a19c67bbeaa4ab92665740355514382aae5a938
zhukoff.dmi <happy-beard@mail.ru>
2023-09-10 17:57:33 +0300

    Исправлена html валидация поисковая форма

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,56 +5,58 @@ import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 function SearchForm({ onSubmit, onToggleClick, shortsActive }) {
-    const location = useLocation();
+  const location = useLocation();
   const [searchString, setSearchString] = useState('');
   const [isSubmitActive, setSubmitActive] = useState(false);
-    const handleMovieChange = (e) => {
+  const handleMovieChange = (e) => {
     setSearchString(e.target.value);
+    setSubmitActive(!!e.target?.value.length);
   };
 
-    const handleSearchSubmit = (e) => {
-        e.preventDefault();
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
     if (localStorage.getItem('inputMovies') !== searchString) {
       if (location.pathname === '/movies') {
         localStorage.setItem('inputMovies', JSON.stringify(searchString));
-        }
+      }
       onSubmit(searchString);
     }
   };
 
-    useEffect(() => {
+  useEffect(() => {
     if (location.pathname === '/movies') {
-      const inputValue = JSON.parse(localStorage.getItem('inputMovies') || '""');
+      const inputValue = JSON.parse(
+        localStorage.getItem('inputMovies') || '""');
       setSearchString(inputValue);
-        }
-    }, [location.pathname]);
+      setSubmitActive(!!inputValue?.length);
+    }
+  }, [location.pathname]);
 
-    return (
-        <section className="search">
-            <form
-                onSubmit={handleSearchSubmit}
-                className="search-form">
-                <div className="search__input">
-                    <input
-                        onChange={handleMovieChange}
-                        className="search-form__input"
-                        type="text"
-                        placeholder="Фильм"
-            // minLength={2}
-            // maxLength={40}
-            // required={true}
+  return (
+    <section className="search">
+      <form
+        onSubmit={handleSearchSubmit}
+        className="search-form">
+        <div className="search__input">
+          <input
+            onChange={handleMovieChange}
+            className="search-form__input"
+            type="text"
+            placeholder="Фильм"
             value={searchString}/>
-                    <button
-                        className="search-form__button"
-                        type="submit">
-                        Поиск</button>
-                </div>
-            </form>
-            <FilterCheckbox
+          <button
+            disabled={!isSubmitActive}
+            className="search-form__button"
+            type="submit">
+            Поиск
+          </button>
+        </div>
+      </form>
+      <FilterCheckbox
         shortsActive={shortsActive}
-                onToggleClick={onToggleClick}
-            />
-        </section>
+        onToggleClick={onToggleClick}
+      />
+    </section>
   );
 }
 
